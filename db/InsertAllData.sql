93465ac38b1c365473a14eb95ba6cfaef1ed547c
Aaron Cui <aaroncui26@hotmail.com>
2018-03-15 14:49:32 -0700

    starter file for insert data sql, not complete

---
+++ db/InsertAllData.sql
@@ -0,0 +1,14 @@
+INSERT INTO Airplane_1(model, business_cap)
+VALUES('BOEING777', 20);
+
+INSERT INTO Airplane_1(model, business_cap)
+VALUES('AIRBUS320', 30);
+
+INSERT INTO Airplane_1(model, business_cap)
+VALUES('AIRBUS330', 24);
+
+INSERT INTO Airplane_1(model, business_cap)
+VALUES('AIRBUS350', 36);
+
+INSERT INTO Airplane_1(model, business_cap)
+VALUES('BOEING787', 24);
