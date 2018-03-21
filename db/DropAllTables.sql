77368547c0e6604e0ce642a76e15eb5f60e61ca2
Aaron Cui <aaroncui26@hotmail.com>
2018-03-21 12:10:57 -0700

    passwords for customers / employees, drop table queries

---
+++ db/DropAllTables.sql
@@ -0,0 +1,52 @@
+/*
+  DROP ALL TABLES
+
+  Orders are changed such that there's no referenced tables to be deleted causing error
+  Execute the entire file should work.
+
+ */
+
+/*
+  NOTE: Try this first for dropping all tables on conventional postgres setup
+  public is the default schema in a database which holds all tables
+ */
+
+DROP SCHEMA public CASCADE;
+CREATE SCHEMA public;
+
+/*
+  Otherwise, try to see which tables to delete, if unable due to references
+  try executing in order to the point of necessity
+ */
+
+-- Non core tables
+DROP TABLE IF EXISTS Reserves_Promotion_Item;
+
+DROP TABLE IF EXISTS Promotion_Item;
+
+DROP TABLE IF EXISTS Staff;
+
+-- References are mostly the top 5 tables here
+DROP TABLE IF EXISTS Reserves_Occupies_Reservation;
+
+DROP TABLE IF EXISTS Customer;
+
+DROP TABLE IF EXISTS Has_Seats_4;
+
+DROP TABLE IF EXISTS LandsAt_TakesOff_Flight;
+
+DROP TABLE IF EXISTS Airport;
+
+DROP TABLE IF EXISTS Airplane_4;
+
+DROP TABLE IF EXISTS Airplane_3;
+
+DROP TABLE IF EXISTS Airplane_2;
+
+DROP TABLE IF EXISTS Airplane_1;
+
+DROP TABLE IF EXISTS Has_Seats_3;
+
+DROP TABLE IF EXISTS Has_Seats_2;
+
+DROP TABLE IF EXISTS Has_Seats_1;
--- db/InsertAllData.sql
+++ db/InsertAllData.sql
@@ -67,76 +67,76 @@ INSERT INTO Airplane_4(plane_id, flight_no, airline, date, model)
 VALUES(9001, '100', 'DL', '2018-05-26', 'AIRBUS350');
 
 -- AIRPORTS
-INSERT INTO Airport_2(code, country, city, capacity)
+INSERT INTO Airport(code, country, city, capacity)
 VALUES('YVR', 'CA', 'Vancouver', 150);
 
-INSERT INTO Airport_2(code, country, city, capacity)
+INSERT INTO Airport(code, country, city, capacity)
 VALUES('ATL', 'US', 'Georgia', 120);
 
-INSERT INTO Airport_2(code, country, city, capacity)
+INSERT INTO Airport(code, country, city, capacity)
 VALUES('CDG', 'FR', 'Paris', 80);
 
-INSERT INTO Airport_2(code, country, city, capacity)
+INSERT INTO Airport(code, country, city, capacity)
 VALUES('HND', 'JP', 'Tokyo', 120);
 
-INSERT INTO Airport_2(code, country, city, capacity)
+INSERT INTO Airport(code, country, city, capacity)
 VALUES('PEK', 'CN', 'Beijing', 150);
 
-INSERT INTO Airport_2(code, country, city, capacity)
+INSERT INTO Airport(code, country, city, capacity)
 VALUES('SFO', 'US', 'San Francisco', 300);
 
-INSERT INTO Airport_2(code, country, city, capacity)
+INSERT INTO Airport(code, country, city, capacity)
 VALUES('LAX', 'US', 'Los Angeles', 1000);
 
-INSERT INTO Airport_2(code, country, city, capacity)
+INSERT INTO Airport(code, country, city, capacity)
 VALUES('NRT', 'JP', 'Narita', 300);
 
-INSERT INTO Airport_2(code, country, city, capacity)
+INSERT INTO Airport(code, country, city, capacity)
 VALUES('JFK', 'US', 'New York City', 900);
 
-INSERT INTO Airport_2(code, country, city, capacity)
+INSERT INTO Airport(code, country, city, capacity)
 VALUES('HNL', 'US', 'Honolulu', 300);
 
-INSERT INTO Airport_2(code, country, city, capacity)
+INSERT INTO Airport(code, country, city, capacity)
 VALUES('SIN', 'SG', 'Changi', 450);
 
-INSERT INTO Airport_2(code, country, city, capacity)
+INSERT INTO Airport(code, country, city, capacity)
 VALUES('HKG', 'HK', 'Hong Kong', 800);
 
-INSERT INTO Airport_2(code, country, city, capacity)
+INSERT INTO Airport(code, country, city, capacity)
 VALUES('YYZ', 'CA', 'Toronto', 700);
 
 -- CUSTOMERS
-INSERT INTO Customer (email, phone_no, freq_flyer_miles)
-VALUES('j.bean@gm.com', '604-999-2222', 2000);
+INSERT INTO Customer (email, password, phone_no, freq_flyer_miles)
+VALUES('j.bean@gm.com', 'password', '604-999-2222', 2000);
 
-INSERT INTO Customer (email, phone_no, freq_flyer_miles)
-VALUES('aaroniscool@wut.com', '778-123-1234', 0);
+INSERT INTO Customer (email, password, phone_no, freq_flyer_miles)
+VALUES('aaroniscool@wut.com', 'hunter123', '778-123-1234', 0);
 
-INSERT INTO Customer (email, phone_no, freq_flyer_miles)
-VALUES('kevinSmart@ubc.com', '555-555-5555', 5000);
+INSERT INTO Customer (email, password, phone_no, freq_flyer_miles)
+VALUES('kevinSmart@ubc.com', '1234', '555-555-5555', 5000);
 
-INSERT INTO Customer (email, phone_no, freq_flyer_miles)
-VALUES('handsomeBrian@smooth.com', '123-456-7890', 1500);
+INSERT INTO Customer (email, password, phone_no, freq_flyer_miles)
+VALUES('handsomeBrian@smooth.com', 'sup', '123-456-7890', 1500);
 
-INSERT INTO Customer (email, phone_no, freq_flyer_miles)
-VALUES('bestAngela@lol.com', '444-444-4444', 100);
+INSERT INTO Customer (email, password, phone_no, freq_flyer_miles)
+VALUES('bestAngela@lol.com', 'angela', '444-444-4444', 100);
 
 -- STAFF
-INSERT INTO Staff (email, sin, name, level)
-VALUES ('Users1@airplaneUsers.com', 111222333, 'Jane', 1);
+INSERT INTO Staff (email, password, sin, name, level)
+VALUES ('Users1@airplaneUsers.com', 'admin', 111222333, 'Jane', 1);
 
-INSERT INTO Staff (email, sin, name, level)
-VALUES ('Users2@airplaneUsers.com', 222111333, 'John', 2);
+INSERT INTO Staff (email, password, sin, name, level)
+VALUES ('Users2@airplaneUsers.com', 'admin', 222111333, 'John', 2);
 
-INSERT INTO Staff (email, sin, name, level)
-VALUES ('jimHardworker3@airplaneUsers.com', 333222111, 'Jim', 3);
+INSERT INTO Staff (email, password, sin, name, level)
+VALUES ('jimHardworker3@airplaneUsers.com', 'admin', 333222111, 'Jim', 3);
 
-INSERT INTO Staff (email, sin, name, level)
-VALUES ('kevin1@airplaneUsers.com', 123456789, 'Kevin', 1);
+INSERT INTO Staff (email, password, sin, name, level)
+VALUES ('kevin1@airplaneUsers.com', 'admin', 123456789, 'Kevin', 1);
 
-INSERT INTO Staff (email, sin, name, level)
-VALUES ('smithLazy@airplaneUsers.com', 987654321, 'Smith', 2);
+INSERT INTO Staff (email, password, sin, name, level)
+VALUES ('smithLazy@airplaneUsers.com', 'admin', 987654321, 'Smith', 2);
 
 -- LANDSAT_TAKESOFF_FLIGHT
 INSERT INTO LandsAt_TakesOff_Flight (flight_no, airline, date, plane_id, landsAt_airport, takesOff_airport)
