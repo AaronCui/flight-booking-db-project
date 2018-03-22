b2c602507b8447df9b5654e30830a8b7375d207c
Aaron Cui <aaroncui26@hotmail.com>
2018-03-22 10:42:39 -0700

    add a link to direct to this sample flight page on main

--- pages/index.vue
+++ pages/index.vue
@@ -16,6 +16,9 @@
       <div class="links">
         <button type="button" class="button--grey" style="margin-left: 15px;" @click="login()">Login</button>
       </div>
+      <nuxt-link :to="{ path: `/flights`,}">
+        CLICK HERE TO SEE LE FLIGHTS BABY
+      </nuxt-link>
     </div>
   </section>
 </template>
